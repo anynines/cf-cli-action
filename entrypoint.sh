bf9515a296eaf404f40fe57d16eb4fa9e659ce6c
Samuel <skretzschmann@anynines.com>
2025-03-28 10:42:01 +0100

    Add retry functionality for cf cli actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,30 @@
 #!/bin/sh -l
 
-cf api "$INPUT_CF_API"
-cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
+: ${INPUT_RETRIES:=3}
 
-if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
-  cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
-fi
+attempt=1
 
-sh -c "cf8 $*"
+while [ $attempt -le $INPUT_RETRIES ]; do
+
+  cf8 api "$INPUT_CF_API"
+  cf8 auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
+
+  if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
+    cf8 target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
+  fi
+
+  sh -c "cf8 $*"
+
+  if [ $? -eq 0 ]; then
+    echo "Deployment Succesful."
+    break
+  else
+    echo "Failed, Attempt $attempt of $INPUT_RETRIES."
+    attempt=$((attempt + 1))
+
+    if [ $attempt -gt $INPUT_RETRIES ]; then
+      echo "Maximum retry attempts reached. Exiting."
+      exit 1
+    fi
+  fi
+done
