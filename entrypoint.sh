ec12ceb4268e7607046c81cffae76c27e03a80c4
Samuel <skretzschmann@anynines.com>
2025-04-07 15:02:58 +0200

    change entrypoint, action.yml for inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/sh -l
 
+
 : ${INPUT_RETRIES:=3}
 
-attempt=1
-env
+INPUT_CF_API=$(echo "$1" | jq '.cf_api')
+INPUT_CF_USERNAME=$(echo "$1" | jq '.cf_username')
+INPUT_CF_PASSWORD=$(echo "$1" | jq '.cf_password')
+INPUT_CF_ORG=$(echo "$1" | jq '.cf_org')
+INPUT_CF_SPACE=$(echo "$1" | jq '.cf_space')
+INPUT_RETRIES=$(echo "$1" | jq '.retries')
+INPUT_COMMAND=$(echo "$1" | jq '.command')
 
-while [ $attempt -le $INPUT_RETRIES ]; do
+attempt=1
+
+while [ $attempt -le "$INPUT_RETRIES" ]; do
 
   cf8 api "$INPUT_CF_API"
   cf8 auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
@@ -14,7 +22,7 @@ while [ $attempt -le $INPUT_RETRIES ]; do
     cf8 target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
   fi
 
-  sh -c "cf8 $*"
+  sh -c "cf8 $INPUT_COMMAND"
 
   if [ $? -eq 0 ]; then
     echo "Deployment Succesful."
@@ -23,7 +31,7 @@ while [ $attempt -le $INPUT_RETRIES ]; do
     echo "Failed, Attempt $attempt of $INPUT_RETRIES."
     attempt=$((attempt + 1))
 
-    if [ $attempt -gt $INPUT_RETRIES ]; then
+    if [ $attempt -gt "$INPUT_RETRIES" ]; then
       echo "Maximum retry attempts reached. Exiting."
       exit 1
     fi
