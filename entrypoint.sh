c1c43674729ffaed292c081caf69d1bce94b45e2
Kristela Kaja <kkaja@anynines.com>
2025-04-14 11:32:04 +0200

    incorporate the changes of the CR

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,37 +2,33 @@
 
 : ${INPUT_RETRIES:=3}
 
-INPUT_CF_API=$(echo "$1" | jq -r '.cf_api')
-INPUT_CF_USERNAME=$(echo "$1" | jq -r '.cf_username')
-INPUT_CF_PASSWORD=$(echo "$1" | jq -r '.cf_password')
-INPUT_CF_ORG=$(echo "$1" | jq -r '.cf_org')
-INPUT_CF_SPACE=$(echo "$1" | jq -r '.cf_space')
-INPUT_RETRIES=$(echo "$1" | jq -r '.retries | tonumber')
-INPUT_COMMAND=$(echo "$1" | jq -r '.command')
+INPUT_CF_API=$(jq -r '.cf_api' <<< "$1")
+INPUT_CF_USERNAME=$(jq -r '.cf_username' <<< "$1")
+INPUT_CF_PASSWORD=$(jq -r '.cf_password' <<< "$1")
+INPUT_CF_ORG=$(jq -r '.cf_org' <<< "$1")
+INPUT_CF_SPACE=$(jq -r '.cf_space' <<< "$1")
+INPUT_RETRIES=$(jq -r '.retries | tonumber' <<< "$1")
+INPUT_COMMAND=$(jq -r '.command' <<< "$1")
+
+cf8 api "$INPUT_CF_API"
+cf8 auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
+
+if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
+  cf8 target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
+fi
 
 attempt=1
-
 while [ $attempt -le "$INPUT_RETRIES" ]; do
-
-  cf8 api "$INPUT_CF_API"
-  cf8 auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
-
-  if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
-    cf8 target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
-  fi
-
   sh -c "cf8 $INPUT_COMMAND"
 
   if [ $? -eq 0 ]; then
     echo "Deployment Succesful."
-    break
-  else
-    echo "Failed, Attempt $attempt of $INPUT_RETRIES."
-    attempt=$((attempt + 1))
-
-    if [ $attempt -gt "$INPUT_RETRIES" ]; then
-      echo "Maximum retry attempts reached. Exiting."
-      exit 1
-    fi
+    exit 0
   fi
-done
+
+  attempt=$((attempt + 1))
+fi
+
+echo "Failed, Attempt $attempt of $INPUT_RETRIES."
+echo "Maximum retry attempts reached. Exiting."
+exit 1
