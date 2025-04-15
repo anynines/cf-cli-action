c2fecec03483b450c709c77010a5de164d168feb
Kristela Kaja <kkaja@anynines.com>
2025-04-15 14:15:09 +0200

    add flag to cf api command

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,21 +9,21 @@ INPUT_RETRIES=$(echo "$1" | jq -r '.retries | try tonumber catch 3')
 INPUT_COMMAND=$(echo "$1" | jq -r '.command')
 INPUT_SKIP_SSL_VALIDATION=$(echo "$1" | jq -r '.skip_ssl_validation')
 
-cf8 api "$INPUT_CF_API"
+CF_CLI_OPTIONS=""
+if [ "$INPUT_SKIP_SSL_VALIDATION" = "true" ]; then
+  CF_CLI_OPTIONS="$CF_CLI_OPTIONS --skip-ssl-validation"
+fi
+
+cf8 api "$INPUT_CF_API $CF_CLI_OPTIONS"
 cf8 auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
 
 if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
   cf8 target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
 fi
 
-CF_CLI_OPTIONS=""
-if [ "$INPUT_SKIP_SSL_VALIDATION" = "true" ]; then
-  CF_CLI_OPTIONS="$CF_CLI_OPTIONS --skip-ssl-validation"
-fi
-
 attempt=1
 while [ $attempt -le "$INPUT_RETRIES" ]; do
-  sh -c "cf8 $INPUT_COMMAND $CF_CLI_OPTIONS"
+  sh -c "cf8 $INPUT_COMMAND"
 
   if [ $? -eq 0 ]; then
     echo "Deployment Succesful."
